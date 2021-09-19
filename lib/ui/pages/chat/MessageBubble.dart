52a3f7861afc0920b3080e3e358ea7d96459ae01
Lino <u201819681@upc.edu.pe>
2021-09-19 14:15:37 -0500

    L.M Adding TimeStamp

--- lib/ui/pages/chat/MessageBubble.dart
+++ lib/ui/pages/chat/MessageBubble.dart
@@ -6,6 +6,22 @@ class MessageBubble extends StatelessWidget {
   final int indx;
   const MessageBubble(this.message, this.indx);
 
+  String parseTime(String time) {
+    var timeparse = time.split(":");
+    print(timeparse);
+
+    var hour = int.parse(timeparse[0]);
+    var decoration;
+    if (hour > 12) {
+      decoration = "pm";
+    } else {
+      decoration = "am";
+    }
+    var newTime = timeparse[0] + ":" + timeparse[1] + " ${decoration}";
+
+    return newTime;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Align(
@@ -24,9 +40,26 @@ class MessageBubble extends StatelessWidget {
                 topLeft:
                     message.isUser ? Radius.circular(10) : Radius.circular(0),
                 bottomRight: Radius.circular(10))),
-        child: Text(
-          message.text,
-          style: TextStyle(color: Colors.white),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text(
+              message.text,
+              style: TextStyle(color: Colors.white),
+            ),
+            Container(
+              width: 55,
+              child: Row(
+                children: [
+                  Text(
+                    parseTime(message.time),
+                    textAlign: TextAlign.end,
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
